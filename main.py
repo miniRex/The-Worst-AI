67843474764d1c8d785c739416a76f07565d9e8c
miniRex <59940124+miniRex@users.noreply.github.com>
2023-03-15 10:39:01 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+
+// 0 1 2 3 4
+//
+//
+//
+//
+
+player = 
+
+print("Hello!")
+
+def drawFrame():
+    print()
+    print("0 0 0 0 0")
+    print("0 0 0 0 0")
+    print("0 0 0 0 0")
+    print("0 0 0 0 0")
+    print("0 0 0 0 0")
+
+drawFrame()
